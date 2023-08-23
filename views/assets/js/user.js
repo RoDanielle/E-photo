20e0875e3a7e5c573173362bfa0f0d3b57955375
Daniel Hay <daniel.hay@cybereason.com>
2023-08-23 13:58:30 +0300

    html

--- views/assets/js/user.js
+++ views/assets/js/user.js
@@ -1,3 +1,9 @@
+/*
+These actions check who the user is currently connected to the site, 
+then displays his information on the screen - in a generic way
+*/
+
+
 const userConsent = true;
 
 if (userConsent) {
@@ -16,24 +22,21 @@ if (userConsent) {
             document.getElementById('email').innerText = `Email: ${user.email}`;
             document.getElementById('location').innerText = `Location: ${user.location}`;
 
-            // הוספת שם המשתמש לתוך ה-canvas
+            // Adding the user's name to the canvas
             const canvas = document.getElementById("userCanvas");
             const ctx = canvas.getContext("2d");
 
             ctx.font = "30px Arial";
-            const nameWidth = ctx.measureText(user.name).width; // מדידת רוחב השם
-            const xPosition = (canvas.width - nameWidth) / 2; // מיקום אופקי מרכזי לשם
+            const nameWidth = ctx.measureText(user.name).width; // Measure the name's width
+            const xPosition = (canvas.width - nameWidth) / 2; // Horizontal centering for the name
 
-            ctx.strokeText(user.name, xPosition, 60); // שם המשתמש
-
-           
+            ctx.strokeText(user.name, xPosition, 60); // User's name
+            
             ctx.font = "20px Arial";
-            const signatureWidth = ctx.measureText("Signature").width; // מדידת רוחב הטקסט
-            const xSignaturePosition = (canvas.width - signatureWidth) / 2 - 20; // מיקום אופקי שמאלי לטקסט
+            const signatureWidth = ctx.measureText("Signature").width; // Measure the text's width
+            const xSignaturePosition = (canvas.width - signatureWidth) / 2 - 20; // Left-aligned horizontal position for the text
 
-            ctx.fillText("Signature:", xSignaturePosition, 30); // הטקסט "Signature"
+            ctx.fillText("Signature:", xSignaturePosition, 30); // The text "Signature"
         })
         .catch(error => console.error('Error fetching user:', error));
 }
-
-
