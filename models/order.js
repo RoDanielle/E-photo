ba94ed013642c110c82d339b39d43e6d22517d18
Shiraz <shirazsorijoun@gmail.com>
2023-08-24 22:11:49 +0300

    orderSchema

---
+++ models/order.js
@@ -0,0 +1,31 @@
+
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+    {
+        id: {
+            type: Number,
+            unique: true,
+        },
+        usersOrder: {
+            type: String,
+        },
+        date: {
+            type: Date,
+            default: Date.now,
+        },
+        cost: {
+            type: Number,
+        },
+        productList: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'Product', // Reference to the Product model
+            }
+        ],
+    }
+);
+
+
+const Order = mongoose.model("order", orderSchema);
+module.exports = Order;
