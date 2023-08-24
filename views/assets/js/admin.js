36525810dc79e3d2ada502dcdbe39047898ef40c
noyaossi <noya.ossi@gmail.com>
2023-08-24 13:19:41 +0300

    addproduct function

--- views/assets/js/admin.js
+++ views/assets/js/admin.js
@@ -1,3 +1,4 @@
+
 // Function to update the navigation bar based on user's login and admin status
 function updateNavbar(isLoggedIn, isAdmin) {
   const storeInfoNavItem = document.getElementById("storeInfoNavItem");
@@ -22,7 +23,52 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+// Add an event listener for form submission
+document.getElementById("addProductForm").addEventListener("submit", async function(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
 
+  const formData = new FormData(event.target);
+  const productData = {
+    name: formData.get("name"),
+    image:formData.get("image"),
+    brand:formData.get("brand"),
+    category:formData.get("category"),
+    price: parseFloat(formData.get("price")),
+    countInStock:formData.get("countInStock"),
+    rating: parseFloat(formData.get("rating")),
+    numReviews:formData.get("numReviews"),
+    description: formData.get("description"),
+    color: formData.get("color"),
+    popularity: formData.get("popularity"),
+  };
+
+  // Call a function to add the product using an API endpoint
+  await addProduct(productData);
+
+  // Clear the form fields after successful product addition
+  event.target.reset();
+});
+
+async function addProduct(productData) {
+  try {
+    const response = await fetch("/api/add-product", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(productData)
+    });
+
+    const data = await response.json();
+    if (data.success) {
+      // Optionally, you can update the product list on the admin page after adding the product
+    } else {
+      console.error("Failed to add product.");
+    }
+  } catch (error) {
+    console.error("Error adding product:", error);
+  }
+}
 
 
 
