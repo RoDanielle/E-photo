0462e53b6dddb6006ff515c5539809541c376b26
Daniel Hay <daniel.hay@cybereason.com>
2023-08-28 17:14:02 +0300

    facebook API2

--- views/assets/js/store-info.js
+++ views/assets/js/store-info.js
@@ -105,41 +105,6 @@ document.getElementById("addProductForm").addEventListener("submit", async funct
 //location.reload();
 
 }
-
-  /*
-  async function addProduct(productData, event) {
-    console.log("inside addProduct");
-    try {
-        const response = await fetch("/api/store-products", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(productData)
-        });
-
-        const data = await response.json();
-        console.log("log:", data);
-
-        if (data.message === 'Product added successfully') {
-            console.log("Product added successfully!");
-
-            // Show an alert to the user
-            alert('Product added successfully!');
-
-            // Reset the form after successful product addition
-            event.target.reset();
-
-            // Refresh the page
-            location.reload();
-        } else {
-            console.error("Failed to add product.");
-        }
-    } catch (error) {
-        console.error("Error adding product:", error);
-    }
-}
-*/
   //Registered users 
     // Fetch user data from MongoDB using an API endpoint
     async function fetchUserData() {
